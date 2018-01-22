ff46c36141a05fcc2e6f4cb917e8d08cf3951bf4
Selak <sergii.lakhtadyr@gmail.com>
2018-01-22 22:11:03 +0100

    change ALLOWED_HOSTS

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zk!+53gyxy&w2t-xpygo!5^%)uyf(tm(l_y&s6wvj(a^si^=$n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['selak.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 #ALLOWED_HOSTS = []
 
 # Application definition
